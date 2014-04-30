007541c466947f8f5aceba2c130f8869cc17a5ea
James Smith <james@floppy.org.uk>
2014-04-30 14:00:41 +0100

    fix envbuilder permissions

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,10 +2,10 @@ default.serverdensity.device_group = "quirkafleeg"
 default.rvm.default_ruby = "1.9.3"
 default.user = "rummager"
 
-default.envbuilder.base_dir = "/home/rummager"
+default.envbuilder.base_dir = "/home/#{default.user}"
 default.envbuilder.base_dbi = "production"
-default.envbuilder.owner = "env"
-default.envbuilder.group = "env"
+default.envbuilder.owner = default.user
+default.envbuilder.group = default.user
 
 default.project_fqdn = "search.theodi.org"
 default.git_project = "rummager"
